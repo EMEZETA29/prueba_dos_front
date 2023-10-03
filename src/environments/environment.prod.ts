7e9550334730e7742819ac80957991d1a42f4637
EMEZETA29 <roberto.pobletet@correoaiep.cl>
2023-10-03 00:19:12 -0300

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  //API_URL:"https://pro401-2.azurewebsites.net",
+  API_URL:"https://cbac-2803-c600-d10e-8561-18e0-be1a-e159-751f.ngrok-free.app/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  //API_URL:"https://pro401-2.azurewebsites.net",
+  API_URL:"https://cbac-2803-c600-d10e-8561-18e0-be1a-e159-751f.ngrok-free.app/"
 };
 
 /*
